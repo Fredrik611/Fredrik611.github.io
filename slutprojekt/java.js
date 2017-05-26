22455cfd9fe6911148a4b5a4834f949af6f63118
Fredrik611 <99fre09@utb.lund.se>
2017-05-26 18:00:12 +0200

    Add files via upload

--- slutprojekt/java.js
+++ slutprojekt/java.js
@@ -1,11 +1,9 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("storbox").style.marginLeft = "250px";
   document.getElementById("storLoggo").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("storbox").style.marginLeft = "0";
